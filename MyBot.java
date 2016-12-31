cf0782bf47586c846f05637832d18419d43a0bd0
CloudyDino <CloudyDino@users.noreply.github.com>
2016-12-31 15:43:56 -0500

    My first submitted bot

--- MyBot.java
+++ MyBot.java
@@ -1,26 +1,52 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class MyBot {
     public static void main(String[] args) throws java.io.IOException {
+        InitPackage iPackage = Networking.getInit();
+        int myID = iPackage.myID;
+        GameMap gameMap = iPackage.map;
 
-        final InitPackage iPackage = Networking.getInit();
-        final int myID = iPackage.myID;
-        final GameMap gameMap = iPackage.map;
-
-        Networking.sendInit("MyJavaBot");
+        Networking.sendInit("DinoBot1");
 
         while(true) {
-            List<Move> moves = new ArrayList<Move>();
+            ArrayList<Move> moves = new ArrayList<Move>();
 
-            Networking.updateFrame(gameMap);
+            gameMap = Networking.getFrame();
+            int width = gameMap.width;
+            int height = gameMap.height;
 
-            for (int y = 0; y < gameMap.height; y++) {
-                for (int x = 0; x < gameMap.width; x++) {
-                    final Location location = gameMap.getLocation(x, y);
-                    final Site site = location.getSite();
-                    if(site.owner == myID) {
-                        moves.add(new Move(location, Direction.randomDirection()));
+            for(int y = 0; y < gameMap.height; y++) {
+                for(int x = 0; x < gameMap.width; x++) {
+                    Site site = gameMap.getSite(new Location(x, y));
+                    Site[] possible = new Site[4];
+                    possible[0] = gameMap.getSite(new Location(x, y-1 + (y==0?height:0)));
+                    possible[1] = gameMap.getSite(new Location(x+1 -((x+1)/width), y));
+                    possible[2] = gameMap.getSite(new Location(x, y+1 - ((y+1)/height)));
+                    possible[3] = gameMap.getSite(new Location(x-1 +(x==0?width:0), y));
+
+                    int toGo = 4;
+                    int toGoStrength = 266;
+                    for (int i=0; i<4; i++) {
+                      if (possible[i].owner != myID && possible[i].strength < toGoStrength)
+                        toGo = i;
+                        toGoStrength = possible[i].strength;
+                    }
+
+                    if (toGo == 4) {
+                      if (site.strength >= site.production*5 || site.strength > 127) {
+                        Direction dir = Direction.CARDINALS[new Random().nextInt(2)];
+                        moves.add(new Move(new Location(x, y), dir));
+                      }
+                      else {
+                        moves.add(new Move(new Location(x, y), Direction.STILL));
+                      }
+                    }
+                    else if (site.strength >= toGoStrength){
+                      moves.add(new Move(new Location(x, y), Direction.CARDINALS[toGo]));
+                    }
+                    else {
+                      moves.add(new Move(new Location(x, y), Direction.STILL));
                     }
                 }
             }
