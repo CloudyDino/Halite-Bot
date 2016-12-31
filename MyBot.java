d3c6a792f61572b584afe71b5ebe81b20def029a
CloudyDino <CloudyDino@users.noreply.github.com>
2016-12-31 15:45:44 -0500

    Submitted bot 2

--- MyBot.java
+++ MyBot.java
@@ -2,12 +2,19 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class MyBot {
+
+    private static int myID;
+    private static GameMap gameMap;
+    private static Random random = new Random();
+    private static Direction randomDirection = Direction.STILL;
+
     public static void main(String[] args) throws java.io.IOException {
         InitPackage iPackage = Networking.getInit();
-        int myID = iPackage.myID;
-        GameMap gameMap = iPackage.map;
+        myID = iPackage.myID;
+        gameMap = iPackage.map;
 
-        Networking.sendInit("DinoBot1");
+
+        Networking.sendInit("DinoBot2");
 
         while(true) {
             ArrayList<Move> moves = new ArrayList<Move>();
@@ -15,42 +22,65 @@ public class MyBot {
             gameMap = Networking.getFrame();
             int width = gameMap.width;
             int height = gameMap.height;
+            randomDirection = Direction.CARDINALS[random.nextInt(2)+2];
 
-            for(int y = 0; y < gameMap.height; y++) {
-                for(int x = 0; x < gameMap.width; x++) {
-                    Site site = gameMap.getSite(new Location(x, y));
-                    Site[] possible = new Site[4];
-                    possible[0] = gameMap.getSite(new Location(x, y-1 + (y==0?height:0)));
-                    possible[1] = gameMap.getSite(new Location(x+1 -((x+1)/width), y));
-                    possible[2] = gameMap.getSite(new Location(x, y+1 - ((y+1)/height)));
-                    possible[3] = gameMap.getSite(new Location(x-1 +(x==0?width:0), y));
-
-                    int toGo = 4;
-                    int toGoStrength = 266;
-                    for (int i=0; i<4; i++) {
-                      if (possible[i].owner != myID && possible[i].strength < toGoStrength)
-                        toGo = i;
-                        toGoStrength = possible[i].strength;
-                    }
-
-                    if (toGo == 4) {
-                      if (site.strength >= site.production*5 || site.strength > 127) {
-                        Direction dir = Direction.CARDINALS[new Random().nextInt(2)];
-                        moves.add(new Move(new Location(x, y), dir));
-                      }
-                      else {
-                        moves.add(new Move(new Location(x, y), Direction.STILL));
-                      }
-                    }
-                    else if (site.strength >= toGoStrength){
-                      moves.add(new Move(new Location(x, y), Direction.CARDINALS[toGo]));
-                    }
-                    else {
-                      moves.add(new Move(new Location(x, y), Direction.STILL));
+            for(int y = 0; y < height; y++) {
+                for(int x = 0; x < width; x++) {
+                    Location location = new Location(x,y);
+                    if (gameMap.getSite(location).owner == myID) {
+                        moves.add(move(location));
                     }
                 }
             }
             Networking.sendFrame(moves);
         }
+    }
+
+    private static Move move(Location location) {
+        Site site = gameMap.getSite(location);
+
+        boolean[] possible = new boolean[4];
+        int numberPossible = 0;
+        int numberOwned = 0;
+
+        for (int i=0; i<4; i++) {
+            Site toGoSite = gameMap.getSite(location,Direction.CARDINALS[i]);
+            if (toGoSite.owner == myID) {
+                numberOwned++;
+            } else if (toGoSite.strength < site.strength) {
+                possible[i] = true;
+                numberPossible++;
+            }
+        }
+        if (numberOwned == 4) {
+            if (site.strength > 6*site.production)
+                return new Move(location, randomDirection);
+            return new Move(location, Direction.STILL);
+        } else if (numberPossible == 0) {
+            return new Move(location, Direction.STILL);
+        } else if (numberPossible == 1) {
+            for (int i=0; i<4; i++) {
+                if (possible[i])
+                    return new Move(location, Direction.CARDINALS[i]);
+            }
+        } else {
+            int toGoProduction = 0;
+            for (int i=0; i<4; i++) {
+                if (possible[i]) {
+                    Site toGoSite = gameMap.getSite(location, Direction.CARDINALS[i]);
+                    if (toGoSite.production > toGoProduction) {
+                        toGoProduction = toGoSite.production;
+                    } else {
+                        possible[i] = false;
+                    }
+                }
+            }
+            for (int i=3; i>=0; i--) {
+                if (possible[i]) {
+                    return new Move(location, Direction.CARDINALS[i]);
+                }
+            }
+        }
+        return new Move(location, Direction.STILL);
     }
 }
